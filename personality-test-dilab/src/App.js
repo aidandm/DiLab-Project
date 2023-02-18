deefc5c7586bc65ea006f6762e1919bda34657b5
aidandm <admully@yahoo.com>
2023-02-17 23:47:58 -0800

    Created mockup with question and continue comps

--- personality-test-dilab/src/App.js
+++ personality-test-dilab/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import QuestionComponent from './components/Questions.jsx';
+import ContinueComponent from './components/Continue.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="heading">Personality Test</h1>
       </header>
+
+      <QuestionComponent question={"I am sensitive to others' emotions"} />
+      <QuestionComponent question={'When engaged in projects, I want to get everything "just right"'} />
+      <QuestionComponent question={"I am sensitive to others' emotions"} />
+      <QuestionComponent question={"I stay calm under pressure"} />
+      <QuestionComponent question={"I get stressed out easily"} />
+      <ContinueComponent />
+
     </div>
   );
 }
